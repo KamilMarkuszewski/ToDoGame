e844e9fc79d7a1b059b1b1768db06c3c739f0c7a
KamilMarkuszewski <kamilmarkuszewski89@gmail.com>
2019-12-30 10:16:58 +0100

    add heroo, to do list, achevements components

--- ToDoGame/src/app/app.module.ts
+++ ToDoGame/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HeroComponent } from './components/hero/hero.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    ToDoListComponent,
+    AchievementsComponent
   ],
   imports: [
     BrowserModule,
